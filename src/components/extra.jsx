67cbc81ea6ed8266ec21cbc43c90dea530b02d44
Anil Kumar Behera <ark845612@gmail.com>
2023-01-06 00:01:17 +0530

    extra added, minor css change

---
+++ src/components/extra.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+const Extra = () => {
+    return ( 
+        <div className="container">
+            <div className="row">
+                <div className="col m-15 p-3 panel">
+                    <div className="row">
+                        <div className="col"><p>Welcome! Are you new here? Create an account</p></div>
+                        <div className="col">
+                        <Link to="/signUp">
+                        <button className="btn btn-danger">Sign Up</button>
+                        </Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="col offset-4 m-15 p-3 panel">
+                    <div className="row">
+                        <div className="col"><p>Already have an account?</p></div>
+                        <div className="col">
+                        <Link to="/login">   
+                            <button className="btn btn-danger">Login</button>
+                        </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+         </div>
+    );
+}
+ 
+export default Extra;
